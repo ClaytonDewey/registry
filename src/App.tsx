9d9a199f1f9e795a013e56d28f67575a8d044ad2
ClaytonDewey <clay@dryadmedia.com>
2024-05-14 15:21:51 -0500

    Set fetch data function

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,21 @@ const router = createBrowserRouter([
       {
         path: '/search',
         element: <SearchPage />,
+        loader: async ({ request }) => {
+          // https://registry.npmjs.org/<package name>
+          const { searchParams } = new URL(request.url);
+          const term = searchParams.get('term');
+
+          if (!term) {
+            throw new Error('Search term must be provided.');
+          }
+
+          const res = await fetch(
+            `https://registry.npmjs.org/-/v1/search?text=${term}`
+          );
+          const data = await res.json();
+          return data.objects;
+        },
       },
       {
         path: '/packages/:name',
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,9 @@
+import { useLoaderData } from 'react-router-dom';
+
 const SearchPage = () => {
+  const data = useLoaderData();
+  console.log(data);
+
   return <div>SearchPage</div>;
 };
 export default SearchPage;
