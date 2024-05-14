8c8d8b8d87e78c15e6d342a261274e65128bc1f2
ClaytonDewey <clay@dryadmedia.com>
2024-05-14 15:04:09 -0500

    Add Styling to header

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { VscSearch } from 'react-icons/vsc';
 
 const SearchInput = () => {
   const [term, setTerm] = useState('');
@@ -13,11 +14,23 @@ const SearchInput = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type='search'
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
-      />
+      <label htmlFor='search' className='sr-only'>
+        Search
+      </label>
+      <div className='relative'>
+        <div className='absolute inset-y-0 flex items-center pl-3'>
+          <VscSearch className='h-5 w-5 text-gray-500' />
+        </div>
+        <input
+          className='pl-10 py-2 w-full border-0 shadow-none'
+          id='search'
+          name='search'
+          type='search'
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          placeholder='Search packagers'
+        />
+      </div>
     </form>
   );
 };
